7535b2c68d5d3df0f490cd43187425b7ffed2e87
ByteGenius007 <batirhan.tai@gmail.com>
2026-02-05 01:32:23 +0500

    lj

--- config.py
+++ config.py
@@ -6,10 +6,8 @@ load_dotenv()
 BOTS = {
     "marat": os.getenv("BOT_MARAT"),
 }
-ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN")
 
-ADMIN_ID = os.getenv("ADMIN_ID")
+ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))  # важно привести к int
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-
