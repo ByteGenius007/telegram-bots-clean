e82c8345063965c09410ef587ee94f4b608384a4
ByteGenius007 <batirhan.tai@gmail.com>
2026-02-08 18:17:59 +0500

    llllol

--- keyboards/menu_kb.py
+++ keyboards/menu_kb.py
@@ -4,13 +4,13 @@ main_menu_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [
             InlineKeyboardButton(
-                text="📖 Подробнее",
+                text="📖 Подробнее о компании",
                 callback_data="about_company"
             )
         ],
         [
             InlineKeyboardButton(
-                text="🤖 Узнать",
+                text="🤖 Узнать о продукции",
                 callback_data="products"
             )
         ]
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from bots.marat_bot import dp as dp_marat, bot as bot_marat
 from bots.admin_bot import dp as dp_admin, bot as bot_admin
 import os
 import sys
+from database import init_db
 async def main():
     if os.getenv("BOT_ENABLED") == "false":
         print("Bot disabled")
         sys.exit()
-
+    
+    await init_db()
     # запускаем оба бота параллельно
     await asyncio.gather(
         dp_marat.start_polling(bot_marat),
